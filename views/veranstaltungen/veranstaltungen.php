78e06cf4a354a13f78e68194aab3c48eb9197f49
f <f>
2012-12-04 18:21:26 +0100

    ich bins sascha hi

--- views/veranstaltungen/veranstaltungen.php
+++ views/veranstaltungen/veranstaltungen.php
@@ -1,28 +1,14 @@
 <?php
- ob_start();
- 
+
 /**
  * FHD-App
  *
  * @version 0.0.1
  * @copyright Fachhochschule Duesseldorf, 2012
  * @link http://www.fh-duesseldorf.de
- * @author Sascha Moeller (FM), <sascha.moeller@fh-duesseldorf.de>
+ * @author Fabian Martinovic (FM), <fabian.martinovic@fh-duesseldorf.de>
  */
 
  
 /* End of file veranstaltungen.php */
 /* Location: ./views/veranstaltungen/veranstaltungen.php */
-
-	//Header
-	require_once '../../layout/frontend/header.php';
-		
-	//Content - Veranstaltungen
-	
-	echo 'CONTENT';
-	
-	//Ende Content - Veranstaltungen
-	require_once '../../layout/frontend/header.php';
-	
-	ob_end_flush();
-?>
